3b755a6d3c2b3e5d6de62b6e490a9af15768c8dd
robana07 <robana07@ccm-2610-f2.evergreen.edu>
2013-11-14 16:56:30 -0800

    Their hw6

---
+++ hw6.py
@@ -0,0 +1,75 @@
+# Name: ...
+# Evergreen Login: ...
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 6
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 3
+###
+ages = {'Nicholas' : '28' , 'Mary' : '21' , 'Ulises' : '14'}
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+print ages
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 4
+###
+def date_of_birth(name):
+    dob = 2013 - name
+    print "She was born in " + str(dob) + "."
+    print ages
+    
+#What would you put in this line to call the function date_of_birth to find out
+#Mary's birth year?
+
+date_of_birth(Mary)
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 7
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 7 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
